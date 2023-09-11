92dee540ee95dae9e91d0b97390341849049f278
Lars Klitzke <lars.klitzke@gmail.com>
2023-09-11 13:56:48 +0200

    cleanup

--- xodr2nxgpd/__init__.py
+++ xodr2nxgpd/__init__.py
@@ -5,17 +5,13 @@ import logging
 logging.basicConfig()
 
 if sys.prefix in os.path.abspath(__file__):
-    # the package was installed
-    logging.info('Prepare to run in deployment mode')
     ROOT_PATH = os.path.join(sys.prefix, 'etc', os.path.basename(os.path.dirname(__file__)))
 
     if not os.path.exists(ROOT_PATH):
         ROOT_PATH = os.path.join(sys.prefix, 'local', 'etc', os.path.basename(os.path.dirname(__file__)))
 
     if not os.path.exists(ROOT_PATH):
-        raise RuntimeError('Failed to find tasi supplementary data in system prefix %s.', sys.prefix)
+        raise RuntimeError('Failed to find supplementary data in system prefix %s.', sys.prefix)
 
 else:
-    logging.info('Prepare to run in development mode')
-    # we are in development mode
     ROOT_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'etc')
